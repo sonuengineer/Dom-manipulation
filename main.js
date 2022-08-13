5f4ee6cb445ed24f8dc2ed431cd308c5a720f7ad
sonuengineer <prajapatisonu50@gmail.com>
2022-08-13 21:12:20 +0530

    li bold in color and 3rd elemrnt in green color

--- main.js
+++ main.js
@@ -6,4 +6,14 @@ var d=document.getElementById("ma")
 console.log(d)
 d.style.fontWeight="bold"
 
+var c=document.getElementsByTagName("li")
+
+c[2].style.backgroundColor="green"
+
+
+
+for(var i=0;i<4;i++){
+    c[i].style.fontWeight="bold"
+}
+
 
