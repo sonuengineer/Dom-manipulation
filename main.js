23ffd8ba9ef3d50f1acee9c6035ed490c06ee650
sonuengineer <prajapatisonu50@gmail.com>
2022-08-13 21:05:24 +0530

    add changes in font bold

--- main.js
+++ main.js
@@ -6,13 +6,4 @@ var d=document.getElementById("ma")
 console.log(d)
 d.style.fontWeight="bold"
 
-var c=document.getElementsByTagName("li")
-
-c[2].style.backgroundColor="green"
-
-
-
-for(var i=0;i<4;i++){
-    c[i].style.fontWeight="bold"
-}
 
