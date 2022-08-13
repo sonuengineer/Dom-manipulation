0c2149b2b0be006f6703eb74300501f8539f6b0c
sonuengineer <prajapatisonu50@gmail.com>
2022-08-13 21:03:00 +0530

    ADD ITEM bold and chage the font color to greeen

---
+++ main.js
@@ -0,0 +1,18 @@
+var a=document.getElementById("header-title")
+a.style.borderBottom='solid 3px black'
+
+
+var d=document.getElementById("ma")
+console.log(d)
+d.style.fontWeight="bold"
+
+var c=document.getElementsByTagName("li")
+
+c[2].style.backgroundColor="green"
+
+
+
+for(var i=0;i<4;i++){
+    c[i].style.fontWeight="bold"
+}
+
