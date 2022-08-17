6dcd247a8db2c862af9939b9a7587fd0d4a60041
sonuengineer <prajapatisonu50@gmail.com>
2022-08-17 19:44:15 +0530

    queryselector

--- main.js
+++ main.js
@@ -1,19 +1,5 @@
-var a=document.getElementById("header-title")
-a.style.borderBottom='solid 3px black'
 
+var t1=document.querySelector(".list-group")
+t1[1].style.backgroundColor="green"
 
-var d=document.getElementById("ma")
-console.log(d)
-d.style.fontWeight="bold"
-
-var c=document.getElementsByTagName("li")
-
-c[2].style.backgroundColor="green"
-
-
-
-for(var i=0;i<4;i++){
-    c[i].style.fontWeight="bold"
-}
-
-
+t1[2].style.display=none
